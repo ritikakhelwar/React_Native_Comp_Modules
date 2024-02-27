0b0cad49696b189ea30368802fe5c0f43196f3e5
ritikakhelwar <54799149+ritikakhelwar@users.noreply.github.com>
2024-02-27 15:55:30 +0530

    Update Upload_Media_File_In_chunks.js

--- Upload_Media_File_In_chunks.js
+++ Upload_Media_File_In_chunks.js
@@ -8,7 +8,83 @@ import {TUS_URL} from '../../config/urls';
 interface ReelsTabProps {}
 
 const ReelsTab: React.FC<ReelsTabProps> = () => {
+// while uploading from ios device or android device using cameraRoll library
 
+  /*if (Platform.OS === 'ios') {
+          CameraRoll.iosGetImageDataById(currentImage?.image?.uri)
+            .then(res => {
+              console.log('uploadPath', res);
+              if (!res?.node?.image?.filepath) {
+                return;
+              }
+              let uploadPath = res?.node?.image?.filepath;
+              uploadMediaInChunks({...currentImage, uri: uploadPath});
+            })
+            .catch(err => {
+              console.log('uploadPath err', err);
+            });
+        } else {
+          uploadMediaInChunks({...currentImage, uri: currentImage?.image?.uri});
+        }*/
+
+// upload function parameters while uploading using cameraRoll on ios and adnroid
+/*
+export const uploadMediaInChunks = (value, apiPayload) => {
+  return new Promise((resolve, reject) => {
+    console.log('valuevaluevaluevaluevalue', value);
+    const file = {
+      ...value,
+      type:
+        Platform.OS == 'android'
+          ? value?.type
+          : `${value?.type}/${value?.image?.extension}`,
+    };
+    // console.log('Im here33333', file);
+    var upload = new tus.Upload(file, {
+      endpoint: TUS_URL,
+      retryDelays: [0, 3000, 5000, 10000, 20000],
+      metadata: {
+        filename: file.filename || 'filename',
+        filetype: file.type,
+      },
+
+      onError: function (error) {
+        console.log('Failed because: ' + error);
+        reject(error);
+      },
+      onProgress: function (bytesUploaded, bytesTotal) {
+        var percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
+        // console.log(bytesUploaded, bytesTotal, percentage + '%');
+        actions.saveMediaProgressToStore({
+          bytesUploaded: bytesUploaded,
+          bytesTotal: bytesTotal,
+          percentage: percentage,
+          file: file,
+        });
+      },
+      onSuccess: function (res) {
+        console.log('Downloaddddd ', upload?.url);
+        resolve({url: upload?.url});
+        actions.saveMediaProgressToStore(null);
+
+        actions.savePostParamDataToStore({
+          file_url: upload?.url,
+          postItem: true,
+        });
+      },
+    });
+    // Check if there are any previous uploads to continue.
+    upload.findPreviousUploads().then(function (previousUploads) {
+      // Found previous uploads so we select the first one.
+      if (previousUploads?.length) {
+        upload.resumeFromPreviousUpload(previousUploads[0]);
+      }
+      // Start the upload
+      upload.start();
+    });
+  });
+};
+*/
 
   const selectSingleImageFromGallery = () => {
   return new Promise(async (resolve, reject) => {
